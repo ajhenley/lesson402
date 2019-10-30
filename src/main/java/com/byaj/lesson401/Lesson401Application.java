de12c338e91dcc99e2b57c8f7a9ea13ed0c17b7e
AJ Henley <ajhenley@gmail.com>
2019-10-29 20:45:58 -0400

    Initial commit

---
+++ src/main/java/com/byaj/lesson401/Lesson401Application.java
@@ -0,0 +1,13 @@
+package com.byaj.lesson401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson401Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Lesson401Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/byaj/lesson401/Lesson401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byaj.lesson401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson401ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
